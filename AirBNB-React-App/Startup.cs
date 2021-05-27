d2b8f90cc0dbb9ee51cddb20d21405e8667fc010
Aaron van den Berg <a.vdberg98@gmail.com>
2021-05-27 14:03:49 +0200

    Added AddControllersWithViews service

--- AirBNB-React-App/Startup.cs
+++ AirBNB-React-App/Startup.cs
@@ -24,6 +24,8 @@ namespace AirBNB_React_App
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+
             // services.AddCors();
             // services.AddResponseCompression();
 
