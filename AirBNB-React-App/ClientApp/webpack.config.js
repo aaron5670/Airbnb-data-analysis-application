1333feff8e8204da964c0b7969f59ff716b1652d
Aaron van den Berg <a.vdberg98@gmail.com>
2021-05-28 15:24:34 +0200

    test #6

---
+++ AirBNB-React-App/ClientApp/webpack.config.js
@@ -0,0 +1,19 @@
+﻿var webpack = require('webpack');
+
+module.exports = {
+    use: {
+        loader: 'babel-loader',
+        options: {
+            presets: ['my-custom-babel-preset'],
+            ignore: [ './node_modules/mapbox-gl/dist/mapbox-gl.js' ]
+        }
+    },
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\bmapbox-gl-csp-worker.js\b/i,
+    //             use: { loader: "worker-loader" },
+    //         },
+    //     ],
+    // },
+};
